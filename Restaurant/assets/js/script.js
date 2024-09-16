f048a1757aa386c059bb143dd7baa73642b104ed
enesozmus <ensozmus@gmail.com>
2024-09-16 15:32:50 +0300

    Restaurant | About Section

--- Restaurant/assets/js/script.js
+++ Restaurant/assets/js/script.js
@@ -19,7 +19,6 @@ window.addEventListener("load", function () {
 /**
  * add event listener on multiple elements
  */
-
 const addEventOnElements = function (elements, eventType, callback) {
     for (let i = 0, len = elements.length; i < len; i++) {
         elements[i].addEventListener(eventType, callback);
@@ -30,7 +29,6 @@ const addEventOnElements = function (elements, eventType, callback) {
 /**
  * NAVBAR
  */
-
 const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const overlay = document.querySelector("[data-overlay]");
@@ -47,9 +45,8 @@ addEventOnElements(navTogglers, "click", toggleNavbar);
 /**
  * HEADER & BACK TOP BTN
  */
-
 const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
+// const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 let lastScrollPos = 0;
 
@@ -67,11 +64,11 @@ const hideHeader = function () {
 window.addEventListener("scroll", function () {
   if (window.scrollY >= 50) {
     header.classList.add("active");
-    backTopBtn.classList.add("active");
+    // backTopBtn.classList.add("active");
     hideHeader();
   } else {
     header.classList.remove("active");
-    backTopBtn.classList.remove("active");
+    // backTopBtn.classList.remove("active");
   }
 });
 
@@ -136,3 +133,27 @@ addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseover", function
 addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseout", autoSlide);
 
 window.addEventListener("load", autoSlide);
+
+
+/**
+ * PARALLAX EFFECT
+ */
+const parallaxItems = document.querySelectorAll("[data-parallax-item]");
+
+let x, y;
+
+window.addEventListener("mousemove", function (event) {
+
+  x = (event.clientX / window.innerWidth * 10) - 5;
+  y = (event.clientY / window.innerHeight * 10) - 5;
+
+  // reverse the number eg. 20 -> -20, -5 -> 5
+  x = x - (x * 2);
+  y = y - (y * 2);
+
+  for (let i = 0, len = parallaxItems.length; i < len; i++) {
+    x = x * Number(parallaxItems[i].dataset.parallaxSpeed);
+    y = y * Number(parallaxItems[i].dataset.parallaxSpeed);
+    parallaxItems[i].style.transform = `translate3d(${x}px, ${y}px, 0px)`;
+  }
+});
